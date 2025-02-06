93c355b3c0416feb6f161c81a41b744ef9a0e09c
Manjunath K <makzink40@gmail.com>
2025-02-06 16:09:29 +0530

    delete todo

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,16 @@
 import { useEffect, useState } from "react"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog"
 
-import { getTodosApi } from "@/api/todo"
+import { deleteTodoApi, getTodosApi } from "@/api/todo"
 import { getUsersApi } from "@/api/users"
 import TodoCard from "@/components/pages/home/TodoCard"
 import { TodoCardData, TodoData, User } from "@/interfaces"
@@ -13,6 +23,9 @@ const Home = () => {
 
   const [todoList, setTodoList] = useState<TodoCardData[]>([])
   const navigate = useNavigate()
+  const [isOpen, setIsOpen] = useState(false)
+  const [deleteId, setDeleteId] = useState<string>('')
+  const [isDeleting, setIsDeleting] = useState(false)
   const getTodoList = async () => {
     try {
 
@@ -34,24 +47,66 @@ const Home = () => {
     }
   }
 
+  const handleDelete = async () => {
+    try {
+      setIsDeleting(true)
+      const response = await deleteTodoApi(deleteId)
+
+      setTodoList((prev) => {
+        return prev?.filter((todo) => {
+          return todo.id !== deleteId
+        })
+      })
+
+      console.log(response, "response")
+      console.log(deleteId, "delete id")
+      setDeleteId("")
+      setIsOpen(false)
+
+    } catch (error) {
+      setIsDeleting(false)
+      console.error(error)
+    } finally {
+      setIsDeleting(false)
+    }
+  }
+
   useEffect(() => {
     getTodoList()
   }, [])
 
   const todos = todoList?.map((todo) => {
-    return <TodoCard key={todo.id} {...todo} />
+    return <TodoCard key={todo.id} {...todo} setIsOpen={setIsOpen} setDeleteId={setDeleteId} />
   })
 
   return (
-    <div className="flex flex-col  gap-10">
-      <div className="flex justify-between items-center">
-        <div><input /></div>
-        <div><Button onClick={()=> navigate('/add')}> <Plus />New Task</Button></div>
+    <>
+      <div className="flex flex-col  gap-10">
+        <div className="flex justify-between items-center">
+          <div><input /></div>
+          <div><Button onClick={() => navigate('/add')}> <Plus />New Task</Button></div>
+        </div>
+        <div className="flex gap-4 flex-wrap">
+          {todos}
+        </div>
       </div>
-      <div className="flex gap-4 flex-wrap">
-        {todos}
-      </div>
-    </div>
+      <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              task
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <Button className="bg-red-500" onClick={handleDelete}>{isDeleting ? "Deleting.." : "Delete"}</Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   )
 }
 
