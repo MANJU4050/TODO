cf3dc58fc8aabbccbef03f544536d1b335977755
Manjunath K <makzink40@gmail.com>
2025-02-06 23:25:14 +0530

    online status custom hook

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button"
 import { useAuthContext } from "@/context/authcontext"
+import useOnline from "@/hooks/useOnline"
 import { useNavigate } from "react-router-dom"
 
 const Header = () => {
 
   const { user, logout } = useAuthContext()
   const navigate = useNavigate()
+  const isOnline = useOnline()
 
   const logoutUser = () => {
     logout()
@@ -16,7 +18,11 @@ const Header = () => {
     <div className="w-[100%] bg-slate-600 flex justify-between px-4 h-[70px] items-center">
       <h1 className="text-4xl text-white">TO DO</h1>
       <div className="flex gap-4 justify-center items-center">
-        <div className="bg-gray-700 text-white p-2 rounded">{user}</div>
+        <div className="bg-gray-700 text-white p-2 rounded flex justify-center items-center gap-1">
+          {user}
+          {isOnline ? <div className="w-[10px] h-[10px] bg-green-400 rounded-full"></div> : <div className="w-[10px] h-[10px] bg-red-600 rounded-full"></div>}
+        </div>
+
 
         <Button onClick={logoutUser}>logout</Button>
       </div>
