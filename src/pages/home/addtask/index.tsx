9c8cd47ae06f0557f23e467efae1a5c398bc905f
Manjunath K <makzink40@gmail.com>
2025-02-07 09:48:26 +0530

    form label and errors

--- src/pages/home/addtask/index.tsx
+++ src/pages/home/addtask/index.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button"
 import useTaskManage from "@/hooks/useTaskManage"
 import { ArrowLeft } from "lucide-react"
 import { useNavigate } from "react-router-dom"
+import { Label } from "@/components/ui/label"
 
 const AddTask = () => {
 
@@ -51,31 +52,54 @@ const AddTask = () => {
 
     <div className=" h-full flex justify-center items-center relative">
       <Button className="absolute top-0 left-0" onClick={() => navigate('/')}> <ArrowLeft />back</Button>
-      <div className=" w-[400px] flex flex-col gap-8 px-4 bg-slate-700 rounded-md py-10">
-        <h1 className="text-4xl text-white text-center">ADD NEW TASK</h1>
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6">
-          <input {...register("title")} className="min-h-[40px] px-2 rounded" type="text" placeholder="title" />
-          {errors.title?.message && <p className="text-red-500">{errors.title.message}</p>}
-          <textarea {...register("description")} className="min-h-[40px] p-2 rounded" rows={3} placeholder="description" />
-          {errors.description?.message && <p className="text-red-500">{errors.description.message}</p>}
-          <input {...register("dueDate")} className="min-h-[40px] px-2 rounded" type="date" />
-          {errors.dueDate?.message && <p className="text-red-500">{errors.dueDate.message}</p>}
-          <select {...register("assignedUser")} className="min-h-[40px] px-2 rounded" >
-            <option value=''>select user</option>
-            {userOptions}
-          </select>
-          {errors.assignedUser?.message && <p className="text-red-500">{errors.assignedUser.message}</p>}
-          <select {...register("status")} className="min-h-[40px] px-2 rounded" >
-            <option value='todo'>todo</option>
-            <option value='inProgress'>inProgress</option>
-            <option value='done'>done</option>
-          </select>
-          {errors.status?.message && <p className="text-red-500">{errors.status.message}</p>}
-          <select {...register("priority")} className="min-h-[40px] px-2 rounded" >
-            <option value='high'>high</option>
-            <option value='low'>low</option>
-          </select>
-          {errors.priority?.message && <p className="text-red-500">{errors.priority.message}</p>}
+      <div className=" w-[450px] flex flex-col gap-8 px-4 py-10 bg-slate-700 rounded-md">
+        <h1 className="text-4xl text-white text-center">EDIT TASK</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <Label className="text-white">Title</Label>
+            <input {...register("title")} className="min-h-[40px] px-2 rounded" type="text" placeholder="title" />
+            {errors.title?.message && <p className="text-red-500">{errors.title.message}</p>}
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Label className="text-white">Description</Label>
+            <textarea {...register("description")} className="min-h-[40px] p-2 rounded" rows={3} placeholder="description" />
+            {errors.description?.message && <p className="text-red-500">{errors.description.message}</p>}
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Label className="text-white">Due Date</Label>
+            <input {...register("dueDate")} className="min-h-[40px] px-2 rounded" type="date" />
+            {errors.dueDate?.message && <p className="text-red-500">{errors.dueDate.message}</p>}
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Label className="text-white">Assign to</Label>
+            <select {...register("assignedUser")} className="min-h-[40px] px-2 rounded" >
+              <option value=''>select user</option>
+              {userOptions}
+            </select>
+            {errors.assignedUser?.message && <p className="text-red-500">{errors.assignedUser.message}</p>}
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Label className="text-white">Status</Label>
+            <select {...register("status")} className="min-h-[40px] px-2 rounded" >
+              <option value='todo'>todo</option>
+              <option value='inProgress'>inProgress</option>
+              <option value='done'>done</option>
+            </select>
+            {errors.status?.message && <p className="text-red-500">{errors.status.message}</p>}
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Label className="text-white">Priority</Label>
+            <select {...register("priority")} className="min-h-[40px] px-2 rounded" >
+              <option value='high'>high</option>
+              <option value='low'>low</option>
+            </select>
+            {errors.priority?.message && <p className="text-red-500">{errors.priority.message}</p>}
+          </div>
           <Button type='submit'>{isAdding ? "adding" : "Add task"}</Button>
 
         </form>
